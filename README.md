6e8d1d7a2bad862996479fcc354dda7bcd04d67c
Toyomasa Watarai <toyomasa.watarai@gmail.com>
2025-08-12 14:27:34 +0900

    12 key sensor V3

--- README.md
+++ README.md
@@ -7,19 +7,23 @@ Drum Pad demo for micro:bit
 ## 使用した機材
 * micro:bit
 * Seeed micro:bit用GROVEシールド v2.0
-* SparkFun Touch Shield
+* Seeed 12 key capacitive I2C touch sensor V3
 * eVY1シールド
 
 ## デバイスとの接続
 
 micro:bitと他の部品は以下のように接続してください。
 
-|micro:bit|SparkFun Touch Shield|eVY1シールド|
+|micro:bit|I2C touch sensor|eVY1シールド|
 |---|---|---|
 |P19 (SCL)|SCL||
 |P20 (SDA)|SDA||
 |P0||IO1/TXD (D1)|
-|P14|D2||
+|P14|INT||
 |3V3 (VDD)|3V3|3V3|
 |GND|GND|GND|
+
+## 使用方法
+
+micro:bitの[Python開発ページ](https://python.microbit.org/v/3)にアクセスして、`開く...`ボタンをタップして、main.pyを選択します。`micro:bitに送る`ボタンをタップして、プログラムを書き込みます。
 
--- main.py
+++ main.py
@@ -1,21 +1,60 @@
+# SPDX-FileCopyrightText: 2025 Toyomasa Watarai <toyomasa.watarai@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
 from microbit import *
 from array import array
 
-tbl = array("b", [
-            38, 
-            39, 
-            46, 
-            42,
-            49,
-            48,
-            56,
-            52,
-            36 ])
+note_table = array("b", [
+    60,
+    62, 
+    64, 
+    65,
+    67,
+    69,
+    71,
+    72,
+    74,
+    76,
+    77,
+    79
+])
+
+drum_table = array("b", [
+    35,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48
+])
+
+# channel
+channel_table = array("b", [
+    9,
+    1,
+    2,
+    3,
+    4,
+])
 
 uart.init(baudrate=31250, tx=pin0, rx=None)
-uart.write(bytes([0xB9, 0x07, 0x70]))
+for i in range(1, 16):
+    uart.write(bytes([0xB0 + i, 0x07, 0x7F]))
+uart.write(bytes([0xC1, 1]))
+uart.write(bytes([0xC2, 17]))
+uart.write(bytes([0xC3, 25]))
+uart.write(bytes([0xC4, 33]))
+uart.write(bytes([0xC5, 41]))
 
-I2C_ADRS = 0x5A
+# print('MPR121 test code')
+# for Seeed GROVE 12-key Capacitive I2C Touch Sensor V3
+I2C_ADRS = 0x5B
 i2c.init(freq=400000, sda=pin20, scl=pin19)
 i2c.write(I2C_ADRS, bytes([0x80, 0x63]))
 i2c.write(I2C_ADRS, bytes([0x2B, 0x01]))
@@ -57,14 +96,17 @@ i2c.write(I2C_ADRS, bytes([0x5E, 0x8F]))
 
 pin14.set_pull(pin14.PULL_UP)
 prev_reg_val = 0
+ch = 0
+display.set_pixel(ch, 4, 1)
 
-def on_falling_edge(pin):
+def on_falling_edge(channel):
     global prev_reg_val
     i2c.write(I2C_ADRS, bytes([0]), repeat=True)
     low = int.from_bytes(i2c.read(I2C_ADRS, 1), 'little')
     i2c.write(I2C_ADRS, bytes([1]), repeat=True)
     high = int.from_bytes(i2c.read(I2C_ADRS, 1), 'little')
     reg_val = low | (high << 8)
+    sleep(1)
     i2c.write(I2C_ADRS, bytes([2]), repeat=True)
     low = int.from_bytes(i2c.read(I2C_ADRS, 1), 'little')
     i2c.write(I2C_ADRS, bytes([3]), repeat=True)
@@ -72,21 +114,50 @@ def on_falling_edge(pin):
     oor_val = low | (high << 8)
     if (oor_val != 0):
         reset()
-    for i in range(9):
-        if(reg_val & (1 << i)):
-            if (reg_val & prev_reg_val == False):
-                uart.write(bytes([0x99, tbl[i], 100]))
+    
+    for i in range(12):
+        bit_ptn = reg_val & (1 << i)
+        # if(reg_val & (1 << i)):
+        if (bit_ptn != 0):
+            if (i < 5):
+                display.set_pixel(i, 0, 1)
+            elif (i < 10):
+                 display.set_pixel(i-5, 1, 1)
+            elif (i < 12):
+                 display.set_pixel(i-10, 2, 1)
+            if((bit_ptn & prev_reg_val) == False):
+                if (channel == 9):
+                    uart.write(bytes([0x90 + channel, drum_table[i], 100]))
+                else:
+                    uart.write(bytes([0x90 + channel, note_table[i], 100]))
         else:
-            uart.write(bytes([0x89, tbl[i], 0]))
+            if (i < 5):
+                display.set_pixel(i, 0, 0)
+            elif (i < 10):
+                 display.set_pixel(i-5, 1, 0)
+            elif (i < 12):
+                 display.set_pixel(i-10, 2, 0)
+            if (channel == 9):
+                uart.write(bytes([0x80 + channel, drum_table[i], 0]))
+            else:
+                uart.write(bytes([0x80 + channel, note_table[i], 0]))
     prev_reg_val = reg_val
     return reg_val
 
 while True:
     current_state = pin14.read_digital()
     if current_state == 0:
-        btn = on_falling_edge(pin14)
-    if button_a.is_pressed():
-        sleep(1)
+        btn = on_falling_edge(channel_table[ch])
+    if button_a.was_pressed():
+        ch += 1
+        if (ch >= len(channel_table)):
+            ch = 0
+        for i in range(5):
+            if (i == ch):
+                display.set_pixel(i, 4, 1)
+            else:
+                display.set_pixel(i, 4, 0)
+        sleep(100)
     elif button_b.is_pressed():
-        sleep(1)
-    sleep(100)
+        reset()
+    sleep(1)
